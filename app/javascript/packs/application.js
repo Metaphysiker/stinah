c493bf43e33303ea25e1c033566c552d3777dd38
Metaphysiker <s.raess@me.com>
2019-08-12 10:08:13 +0200

    managed to get bootstrap working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import '../stylesheets/application'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
