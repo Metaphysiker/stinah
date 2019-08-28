fdc40f7738f0f7c01cc540b8321ea81d0a3b966b
Metaphysiker <s.raess@me.com>
2019-08-28 07:03:19 +0200

    changed form

--- app/models/home_offer.rb
+++ app/models/home_offer.rb
@@ -1,2 +1,7 @@
 class HomeOffer < ApplicationRecord
+  validates :firstname, :lastname, :street, :plz, :city,
+            :phone, :email, :year, :experience,
+            :motivation, :plans, :species,
+            :race, :age, :size, :gender, :castrated,
+            :stable, :from_then_on, :privacy_statement, presence: true
 end
