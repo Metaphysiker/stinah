0ae88d9a5708dfa4ba0481b26e3d75a97bc53632
Metaphysiker <s.raess@me.com>
2019-09-25 13:30:16 +0200

    search with animal name

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -6,7 +6,7 @@ class Sponsorship < ApplicationRecord
 
   #validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  scope :sponsorships_ilike, ->(search_term) { where("firstname ILIKE ? OR lastname ILIKE ? OR email ILIKE ?", search_term, search_term, search_term) }
+  scope :sponsorships_ilike, ->(search_term) { joins(:animal).where("sponsorships.firstname ILIKE ? OR sponsorships.lastname ILIKE ? OR sponsorships.email ILIKE ? OR animals.name ILIKE ?", search_term, search_term, search_term, search_term) }
 
   def set_last_check_of_payment
     self.update(last_check_of_payment: Date.today)
