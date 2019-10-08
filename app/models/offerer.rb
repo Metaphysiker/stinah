b3948b69442af5dfeb606f2ce6813a7d9788b3b9
Metaphysiker <s.raess@me.com>
2019-10-08 23:11:21 +0200

    added email validate for home offer

--- app/models/offerer.rb
+++ app/models/offerer.rb
@@ -1,4 +1,11 @@
 class Offerer < ApplicationRecord
+
+  validates :firstname, :lastname, :street, :plz, :city,
+            :phone, :email, :year, :experience,
+            :motivation, :plans, presence: true
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   has_many :home_offers
   accepts_nested_attributes_for :home_offers
 end
