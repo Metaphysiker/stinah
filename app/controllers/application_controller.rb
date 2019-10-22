eb36c34755c2d0d3fafd40806623af25ac0e4195
Metaphysiker <s.raess@me.com>
2019-10-23 00:47:57 +0200

    removed ahoy tracker and admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :track_action
+  #after_action :track_action
 
   protected
 
