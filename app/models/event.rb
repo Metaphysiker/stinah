18cf616a519b1e7bb520c34580b211b9d244c4a3
Metaphysiker <s.raess@me.com>
2019-11-29 09:10:53 +0100

    working on tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   has_one_attached :cover
+  has_rich_text :description
 
   def self.categories
     ['save_the_chicken']
--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -26,23 +26,23 @@ RSpec.describe "event", :type => :feature do
     date_start = DateTime.now + rand(1..100).days
     date_end = date_start + 5.hours
     name = Faker::Book.title
-    description = Faker::Lorem.paragraph
+    description = Faker::Lorem.sentence(word_count: 20)
 
     select_option("#event_start_3i", date_start.day)
     select_option("#event_start_2i", I18n.t("date.month_names")[date_start.month])
     select_option("#event_start_1i", date_start.year)
     select_option("#event_start_4i", date_start.strftime('%H'))
-    select_option("#event_start_5i", date_start.minute)
+    select_option("#event_start_5i", date_start.strftime('%M'))
 
     select_option("#event_end_3i", date_end.day)
     select_option("#event_end_2i", I18n.t("date.month_names")[date_end.month])
     select_option("#event_end_1i", date_end.year)
     select_option("#event_end_4i", date_end.strftime('%H'))
-    select_option("#event_end_5i", date_end.minute)
+    select_option("#event_end_5i", date_end.strftime('%M'))
 
     fill_in "event_name", :with => name
-
-    fill_in "event_description", :with => description
+    #fill_in "event_description", :with => description, disabled: true
+    find('#event_description').click.set(description)
 
     click_button "Event erstellen"
 
@@ -53,6 +53,64 @@ RSpec.describe "event", :type => :feature do
     expect(page).to have_content(name)
 
     visit(events_path)
+
+    expect(page).to have_content(name)
+    expect(page).to have_content(description)
+    expect(page).to have_content(I18n.l(date_start))
+    expect(page).to have_content(I18n.l(date_end))
+
+  end
+
+  it "creates an event for save the chicken and expects it to see on the main page, in index and on the save the chicken site" do
+    visit(new_event_path)
+
+    expect(page).to have_content("Sie müssen sich anmelden oder registrieren, bevor Sie fortfahren können.")
+
+    login_with(@admin)
+
+    visit(new_event_path)
+
+    date_start = DateTime.now + rand(1..100).days
+    date_end = date_start + 5.hours
+    name = Faker::Book.title
+    description = Faker::Lorem.sentence(word_count: 20)
+
+    select_option("#event_start_3i", date_start.day)
+    select_option("#event_start_2i", I18n.t("date.month_names")[date_start.month])
+    select_option("#event_start_1i", date_start.year)
+    select_option("#event_start_4i", date_start.strftime('%H'))
+    select_option("#event_start_5i", date_start.strftime('%M'))
+
+    select_option("#event_end_3i", date_end.day)
+    select_option("#event_end_2i", I18n.t("date.month_names")[date_end.month])
+    select_option("#event_end_1i", date_end.year)
+    select_option("#event_end_4i", date_end.strftime('%H'))
+    select_option("#event_end_5i", date_end.strftime('%M'))
+
+    fill_in "event_name", :with => name
+
+    #fill_in "event_description", :with => description
+    find('#event_description').click.set(description)
+
+    category = I18n.t("save_the_chicken") #I18n.t(Animal.species.sample, count: 1)
+    select_option("#event_category", category)
+
+    click_button "Event erstellen"
+
+    logout
+
+    visit(root_path)
+
+    expect(page).to have_content(name)
+
+    visit(events_path)
+
+    expect(page).to have_content(name)
+    expect(page).to have_content(description)
+    expect(page).to have_content(I18n.l(date_start))
+    expect(page).to have_content(I18n.l(date_end))
+
+    visit(save_the_chicken_path)
 
     expect(page).to have_content(name)
     expect(page).to have_content(description)
